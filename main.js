d31ee48079ad402ec7a3d8812309311e9c48e2ef
jonasjepsen0 <jonasjepsen0@gmail.com>
2026-02-10 09:15:19 +0100

    klassisk funktion

---
+++ main.js
@@ -0,0 +1,15 @@
+function add( a, b){
+  return a + b;
+}
+
+add( 2, 3 );
+
+console.log(add(2, 3));
+
+const navn = "jonas"
+
+function sayHello(navn) {
+  return navn;
+}
+
+console.log("hej " + sayHello(navn));
